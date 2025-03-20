3e9d342ea0c2d7909990e6eb98f0454dd1ef817c
Ronak Bothra <ronakbothraa@gmail.com>
2025-03-20 17:59:08 +0530

    Add the edit button

---
+++ app/components/index.ts
@@ -0,0 +1,6 @@
+import IssueStatusBadge from "./IssueStatusBadge";
+import Link from "./Link";
+import ErrorMessage from "./ErrorMessage";
+import Spinner from "./Spinner";
+
+export { IssueStatusBadge, Link, ErrorMessage, Spinner };
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Table } from "@radix-ui/themes";
-import Link from "../components/Link";
 import { prisma } from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import { Link, IssueStatusBadge } from "@/app/components";
 import IssueActions from "./IssueActions";
 import delay from "delay";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(1000);
-
   return (
     <div>
       <IssueActions />
