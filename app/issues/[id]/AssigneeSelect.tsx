d6b7ba3ffc833e87586dc5a8737bfda67c4e5113
Ronak Bothra <ronakbothraa@gmail.com>
2025-03-22 02:45:44 +0530

    Add assignedIssues to Prisma Schema

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,27 +1,29 @@
 "use client";
 import { User } from "@prisma/client";
 import { Avatar, Flex, Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { Island_Moments } from "next/font/google";
 import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {data: users, error, isLoading} = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get<User[]>("/api/users").then(res => res.data),
+    staleTime: 60*1000,
+    retry: 3
+  })
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  if (error) return null
 
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
-          <Select.Label>Users</Select.Label>
-          {users.map((user) => (
+          <Select.Label>suggestions</Select.Label>
+          {isLoading && <Select.Item value="loading">Loading...</Select.Item>}
+          {users?.map((user) => (
             // <Flex>
             //   <Avatar radius="full" size="2" src={user.image!} fallback="?" />
               <Select.Item key={user.id} value={user.id}>
