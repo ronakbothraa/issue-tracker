af98893289169035f7f3e98424a4e1d69dbe800d
Ronak Bothra <ronakbothraa@gmail.com>
2025-03-24 20:13:25 +0530

    fixing some errors

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextResponse) {
+export async function GET(request: NextRequest) {
     console.log(request)
     const user  = await prisma.user.findMany({ orderBy: { name: 'asc' } })
     return NextResponse.json(user)
